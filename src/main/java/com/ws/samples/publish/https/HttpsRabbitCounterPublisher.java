cdb2c9d1a80798e2f09a3b9ae05ee33e239a7d59
Jose Alberto Rubalcaba Velazquez <jose.rubalcaba@softtek.com>
2016-04-15 16:42:23 -0700

    Finished HttpsRabitCounterPublisher with certificates

--- src/main/java/com/ws/samples/publish/https/HttpsRabbitCounterPublisher.java
+++ src/main/java/com/ws/samples/publish/https/HttpsRabbitCounterPublisher.java
@@ -6,18 +6,35 @@
 package com.ws.samples.publish.https;
 
 import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpsConfigurator;
+import com.sun.net.httpserver.HttpsParameters;
+import com.sun.net.httpserver.HttpsServer;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.security.KeyManagementException;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLEngine;
 import javax.net.ssl.TrustManagerFactory;
+import javax.xml.ws.http.HTTPException;
 
 /**
  *
@@ -55,10 +72,10 @@ public class HttpsRabbitCounterPublisher {
     }
 
     private HttpServer getHttpServer(String ip, int port, String path) {
-        HttpServer server = null; 
+        HttpsServer server = null; 
         try{
             InetSocketAddress inet = new InetSocketAddress(port); 
-            server = HttpServer.create(inet, 5); //2nd arg number of client requests to queue
+            server = HttpsServer.create(inet, 5); //2nd arg number of client requests to queue
             SSLContext cxt = SSLContext.getInstance("TLS"); 
             //password for keystore 
             KeyStore ks = KeyStore.getInstance("JKS"); 
@@ -72,9 +89,119 @@ public class HttpsRabbitCounterPublisher {
             cxt.init(factory.getKeyManagers(), tmf.getTrustManagers(), null);
             
             //create ssl engine and configure https to use it 
-            SSLEngine engine = cxt.createSSLEngine(); 
-            server.
+            final SSLEngine engine = cxt.createSSLEngine(); 
+            server.setHttpsConfigurator(new HttpsConfigurator(cxt){
+                public void configure(HttpsParameters params){
+                    params.setCipherSuites(engine.getEnabledCipherSuites());
+                    params.setProtocols(engine.getEnabledProtocols()); 
+                }
+            });
+            server.setExecutor(null); //default
+            HttpContext httpCxt = server.createContext(path, new MyHttpHandler(this)); 
             
+        }catch(CertificateException ex){
+            System.err.println(ex);
+        }catch(IOException ex){
+            System.err.println(ex);
+        }catch(KeyManagementException ex){
+            System.err.println(ex);
+        }catch(KeyStoreException ex){
+            System.err.println(ex);
+        }catch(NoSuchAlgorithmException ex){
+            System.err.println(ex);
+        }catch(UnrecoverableKeyException ex){
+            System.err.println(ex);
         }
+        return server; 
+    }
+    
+    class MyHttpHandler implements HttpHandler{
+
+        private HttpsRabbitCounterPublisher pub; 
+        private MyHttpHandler(HttpsRabbitCounterPublisher pub){ this.pub = pub; }
+        
+        public void handle(HttpExchange he) throws IOException {
+            String verb = he.getRequestMethod().toUpperCase(); 
+            if(verb.equals("GET"))doGet(he); 
+            else if(verb.equals("POST"))doPost(he);
+            else if(verb.equals("DELETE"))doDelete(he); 
+            else throw new HTTPException(405); //method not allowed
+        }
+
+        private void doGet(HttpExchange he) {
+            Map<Integer, Integer> fibs = pub.getFib();
+            Collection<Integer> list = fib.values();
+            responseToClient(he, list.toString());
+        }
+
+        private void doPost(HttpExchange he) {
+            Map<Integer, Integer> fibs = pub.getFib(); 
+            fibs.clear(); 
+            try{
+                InputStream in = he.getRequestBody(); 
+                byte[] raw = new byte[4096];
+                in.read(raw);
+                String nums = new String(raw); 
+                nums = nums.replace('[', '\0');
+                nums = nums.replace(']', '\0');
+                String[] parts = nums.split(","); 
+                List<Integer> list = new ArrayList<Integer>(); 
+                for(String next : parts){
+                    int n = Integer.parseInt(next.trim()); 
+                    fibs.put(n, countRabbits(n));
+                    list.add(fibs.get(n));
+                }
+                Collection<Integer> col = fibs.values(); 
+                String res = "POSTed: " + fibs.toString(); 
+                responseToClient(he, res);
+            }catch(IOException ex){
+                System.err.println(ex);
+            }
+        }
+
+        private void doDelete(HttpExchange he) {
+            Map<Integer, Integer> fibs = pub.getFib(); 
+            fibs.clear(); 
+            responseToClient(he, "All entries delete");
+        }
+        
+        private void responseToClient(HttpExchange he, String res){
+            try{
+                he.sendResponseHeaders(200, 0); //means: arbitrarily many bytes
+                OutputStream out = he.getResponseBody(); 
+                out.write(res.getBytes());
+                out.flush();
+                he.close(); // close all streams
+            }catch(IOException ex){
+                System.err.println(ex);
+            }
+        }
+
+        private int countRabbits(int n) {
+            n = Math.abs(n);
+            if (n < 2) {
+                return n; // easy cases
+            }
+            Map<Integer, Integer> fibs = pub.getFib();
+            // Return cached values if present.
+            if (fibs.containsKey(n)) {
+                return fibs.get(n);
+            }
+            if (fibs.containsKey(n - 1)
+                    && fibs.containsKey(n - 2)) {
+                fibs.put(n, fibs.get(n - 1) + fibs.get(n - 2));
+                return fibs.get(n);
+            }
+            // Otherwise, compute from scratch, cache, and return.
+            int fib = 1, prev = 0;
+            for (int i = 2; i <= n; i++) {
+                int temp = fib;
+                fib += prev;
+                prev = temp;
+            }
+            fibs.put(n, fib);
+            return fib;
+        }
+        
     }
 }
